9a6400d0558e210cd5d910cd049fe77062952abc
BioWar <petervorotyntsev@gmail.com>
2018-05-24 00:13:04 +0300

    made doc string

--- testmod.py
+++ testmod.py
@@ -3,6 +3,7 @@ import LW8
 
 
 class LW8Tests(unittest.TestCase):
+    """Doc string"""
     def test_input(self):
         for instance in LW8.gems:
             self.assertIsInstance(instance, LW8.Stones)
